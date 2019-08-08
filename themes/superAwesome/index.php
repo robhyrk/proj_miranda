ef29bc93d778db634afd52f0d8cf5fa35834c6fc
Rob H <robh@Mac-mini.local>
2019-08-08 01:10:48 -0400

    test: changed shortcode url for staging site

--- themes/superAwesome/index.php
+++ themes/superAwesome/index.php
@@ -70,7 +70,7 @@
     <section class="form" id="contact">
         <h2>Contact</h2>
         <h3>Get Pricing and Free Marketing Plan</h3>
-        <?php echo do_shortcode( '[contact-form-7 id="24" title="Contact Me"]' );?>
+        <?php echo do_shortcode( '[contact-form-7 id="14" title="Contact Me"]' );?>
     </section>
 </section>
 
